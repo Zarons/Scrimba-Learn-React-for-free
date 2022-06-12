4274d4c56404d51444173d5e09a1f213543ea7d2
Geryenko Hawsen <gery.zarons@gmail.com>
2022-06-12 20:40:30 +0900

    finish project part 1 - markup

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -13,3 +13,23 @@ Hints:
   just like in HTML. In the slide, I have it set to 40px
  */
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+const page = (
+  <>
+    <div>
+      <img src="react-logo.png"></img>
+      <h1>Fun facts about React </h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousand of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  </>
+);
+
+ReactDOM.render(page, document.getElementById("root"));
