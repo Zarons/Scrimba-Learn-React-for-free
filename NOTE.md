5fc8ef7a9a6815fe7331c86da0fe5f8cea5b0f2a
Geryenko Hawsen <gery.zarons@gmail.com>
2022-06-12 20:55:38 +0900

    add NOTE.md and change page into a function

---
+++ NOTE.md
@@ -0,0 +1,3 @@
+#Note
+
+React compononents are written using PascalCase as a function and are called as a HTML tag
--- first-project/src/index.js
+++ first-project/src/index.js
@@ -16,20 +16,38 @@ Hints:
 import React from "react";
 import ReactDOM from "react-dom";
 
-const page = (
-  <>
-    <div>
-      <img src="react-logo.png"></img>
-      <h1>Fun facts about React </h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousand of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
-  </>
-);
+// const page = (
+//   <>
+//     <div>
+//       <img src="react-logo.png"></img>
+//       <h1>Fun facts about React </h1>
+//       <ul>
+//         <li>Was first released in 2013</li>
+//         <li>Was originally created by Jordan Walke</li>
+//         <li>Has well over 100k stars on Github</li>
+//         <li>Is maintained by Facebook</li>
+//         <li>Powers thousand of enterprise apps, including mobile apps</li>
+//       </ul>
+//     </div>
+//   </>
+// );
 
-ReactDOM.render(page, document.getElementById("root"));
+function Page() {
+  return (
+    <>
+      <div>
+        <img src="react-logo.png"></img>
+        <h1>Fun facts about React </h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousand of enterprise apps, including mobile apps</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
